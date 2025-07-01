89b8daad89041afbcc67e23ef456c908fc2adc39
Marcelo Vizcarra <111248644+mavix21@users.noreply.github.com>
2025-06-30 21:51:35 -0500

    Initial commit

---
+++ apps/web/app/(app)/layout.tsx
@@ -0,0 +1,45 @@
+import { Geist, Geist_Mono } from "next/font/google";
+
+import "@myapp/ui/globals.css";
+
+import type { Metadata } from "next";
+
+import { ThemeProvider } from "@/app/_shared/ui/theme-provider";
+
+const fontSans = Geist({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+
+const fontMono = Geist_Mono({
+  subsets: ["latin"],
+  variable: "--font-mono",
+});
+
+export const metadata: Metadata = {
+  title: "Next.js Monorepo Template",
+  description: "Next.js Monorepo Template",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={`${fontSans.variable} ${fontMono.variable} font-sans antialiased`}
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
