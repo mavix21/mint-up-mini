783071c231d57d082fa4a0168be4092bd4d56572
mavix21 <marcevizcarra2109@hotmail.com>
2025-07-04 17:59:43 -0500

    feat: enhance next.config.ts with environment variable loading, hot reloading, and improved webpack configuration

--- apps/mini/next.config.ts
+++ apps/mini/next.config.ts
@@ -1,7 +1,33 @@
+import { fileURLToPath } from "url";
 import type { NextConfig } from "next";
+import createJiti from "jiti";
+
+// Import env files to validate at build time. Use jiti so we can load .ts files in here.
+createJiti(fileURLToPath(import.meta.url))("./src/env");
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /** Enables hot reloading for local packages without a build step */
+  transpilePackages: ["@mint-up/db", "@mint-up/ui"],
+
+  // Silence warnings
+  // https://github.com/WalletConnect/walletconnect-monorepo/issues/1908
+  webpack: (config) => {
+    config.externals.push("pino-pretty", "lokijs", "encoding");
+    return config;
+  },
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
+
+  /** We already do linting and typechecking as separate tasks in CI */
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
 };
 
 export default nextConfig;
