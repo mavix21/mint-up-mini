2a736de302453bc970f5c44ee5854a3f0e534b2a
Gianella Xiomara Coronel Manchego <gianella.coronel@inventiva-solutions.com>
2025-07-04 18:57:16 -0500

    feat(9434075137) add convex files

---
+++ packages/convex/convex/_generated/api.d.ts
@@ -0,0 +1,40 @@
+/* eslint-disable */
+/**
+ * Generated `api` utility.
+ *
+ * THIS CODE IS AUTOMATICALLY GENERATED.
+ *
+ * To regenerate, run `npx convex dev`.
+ * @module
+ */
+
+import type {
+  ApiFromModules,
+  FilterApi,
+  FunctionReference,
+} from "convex/server";
+import type * as tables_linkedAccounts from "../tables/linkedAccounts.js";
+import type * as tables_sessions from "../tables/sessions.js";
+import type * as tables_user from "../tables/user.js";
+
+/**
+ * A utility for referencing Convex functions in your app's API.
+ *
+ * Usage:
+ * ```js
+ * const myFunctionReference = api.myModule.myFunction;
+ * ```
+ */
+declare const fullApi: ApiFromModules<{
+  "tables/linkedAccounts": typeof tables_linkedAccounts;
+  "tables/sessions": typeof tables_sessions;
+  "tables/user": typeof tables_user;
+}>;
+export declare const api: FilterApi<
+  typeof fullApi,
+  FunctionReference<any, "public">
+>;
+export declare const internal: FilterApi<
+  typeof fullApi,
+  FunctionReference<any, "internal">
+>;
