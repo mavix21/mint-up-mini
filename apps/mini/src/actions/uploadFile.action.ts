e1d262b7e8d8440a9ccd2843d47ee68b3994b1e2
mavix21 <marcevizcarra2109@hotmail.com>
2025-07-04 20:49:38 -0500

    feat: implement file upload functionality with Convex API

---
+++ apps/mini/src/actions/uploadFile.action.ts
@@ -0,0 +1,21 @@
+"use server";
+
+import type { Id } from "@mint-up/convex/_generated/dataModel";
+import { fetchMutation } from "@mint-up/convex";
+import { api } from "@mint-up/convex/_generated/api";
+
+export async function uploadFile(file: File) {
+  const uploadUrl = await fetchMutation(api.storage.generateUploadUrl);
+  const res = await fetch(uploadUrl, {
+    method: "POST",
+    headers: { "Content-Type": file.type },
+    body: file,
+  });
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const response = await res.json();
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+  const storageId = response.storageId as Id<"_storage">;
+
+  return { storageId, uploadUrl };
+}
